424b2743d6178a54a8d13389b1c5ca11f19a535a
Gandla <shgandla@paypal.com>
2020-02-23 00:49:37 -0500

    added bootstrap

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './css/style.css'
 
 class App extends React.Component{
   render(){
     return (
-      <div>Hello</div>
+      <div>
+          <a className="btn btn-primary" href="#" role="button">Link</a>
+        
+        </div>
     )
   }
 }
